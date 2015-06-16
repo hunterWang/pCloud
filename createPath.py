347541c7b6f2f5a8758ed6de72d29510935954f9
hunterwang <hunter.wang@iadea.com>
2015-06-16 16:52:52 +0800

    add createPCloud

--- createPath.py
+++ createPath.py
@@ -1,20 +1,25 @@
 #encoding:utf-8
 
 import random
+import sys
 
-def createPath (times):
+
+#---------------------parameter -------------
+times=1 #how many path you want to produce 
+width=20 #boundry
+speed=1  
+period=15 #time have one point 
+time=15  #how much time the point move in second
+maxDistant=speed*period
+radius=5 #equvilant to accurancy for track
+
+
+def createPath ():
 	#---------------------explain -------------
-	# create many fake path inside a width*width 2Dbox
-	# times means how many path you need,
-	# will return a list contain fake path 
+	'''create many fake path inside a width*width 2Dbox
+	times means how many path you need,
+	will return a list contain fake path''' 
 	#---------------------parameter -------------
-	width=20 #boundry
-	speed=1  
-	period=15 #time have one point 
-	time=60  #how much time the point move in second
-	maxDistant=speed*period
-
-	radius=5 #equvilant to accurancy for track
 
 	#---------------------cotroller -------------
 	fakePointOn = 1
@@ -38,17 +43,18 @@ def createPath (times):
 
 		if fakePointOn:
 			for i in range(0,int(time/period)):
-				path[j][i]=fakePoint(path[j][i],width,radius)
+				path[j][i]=fakePoint(path[j][i])
 
 
 
 	return path
 
-def fakePoint (point,width,radius):
+def fakePoint (point):
 	#---------------------explain -------------
-	#point should be [x.y] and will rutnrn a point which 
-	#distant from point in radius you want and inside the box with width  
-	
+	'''
+	point should be [x.y] and will rutnrn a point which 
+	distant from point in radius you want and inside the box with width  
+	'''
 
 	#---------------------parameter -------------
 	# radius = 5 #equvilant to accurancy for track
@@ -65,10 +71,50 @@ def fakePoint (point,width,radius):
 	return [x,y]
 
 
+def createPCloud(path):
+	#---------------------explain -------------
+	'''
+	input is path[0~n] n stand for how many path 
+	each path has certain point like path[0][0]=[x,y],path[0][1]=[x,y].....means every second have a position
+	output will be map[0~n] n will map to path n below
+	each map will like map[0][1][x][y]=0.5 means when time=1 path[0]'s possibility map at (x,y)=0.5    	
+	'''
+	from math import exp,sqrt,pi
+	#---------------------parameter -------------
+	std = 2
+
+	#---------------------start------------------
+	map=[]
+	for n in range(0,times):
+		map.append([])
+		for t in range(0,int(time/period)):
+			map[n].append([])
+			for i in range(0,width):
+				map[n][t].append([])
+				x=path[n][t][0]
+				y=path[n][t][1]
+				for j in range(0,width):
+					rSruare = (x-i)*(x-i)+(y-j)*(y-j)
+					map[n][t][i].append(1/(sqrt(2*pi)*std)*exp(-1*rSruare/(2*std*std)))
+			# map[n][t]=normalizeMap(map[n][t])
 
 
-print createPath(1)
-# print fakePoint([3,4])
+	return map
+
+
+
+
+
+path =  createPath()
+mapP = createPCloud(path)
+
+
+
+for i in range(0,len(mapP[0][0])):
+	for j in range(0,len(mapP[0][0][i])):
+		print "%.3f," %mapP[0][0][i][j],		
+	print "\n"
+
 
 
 
